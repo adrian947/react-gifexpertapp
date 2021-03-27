87a346f09eca782dae139404e9409e53936caac9
Adrian Adducchio <adrian.vhvhvh@gmail.com>
2021-03-27 15:51:12 -0300

    docs agreado

---
+++ Docs/precache-manifest.1c2d1f6b2d02fb8d66838ea55b99b164.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fffd53decd096c0bbcd410ec0675da1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "622b9a1bbe3c4486add9",
+    "url": "/static/css/main.90315c7e.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "622b9a1bbe3c4486add9",
+    "url": "/static/js/main.20560e6a.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
